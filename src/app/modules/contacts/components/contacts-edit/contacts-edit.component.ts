511f3bbc861ac8eca71b1bb37c1e7d89602d22d5
humizi <humizi307@gmail.com>
2021-08-19 21:08:48 +1000

    custom field add

--- src/app/modules/contacts/components/contacts-edit/contacts-edit.component.ts
+++ src/app/modules/contacts/components/contacts-edit/contacts-edit.component.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Component, Inject } from '@angular/core';
 import { ICONTACTS_SERVICE } from 'src/app/services/contacts/contacts-service.interface';
@@ -10,31 +10,33 @@ import { Router } from '@angular/router';
   templateUrl: './contacts-edit.component.html'
 })
 export class ContactsEditComponent {
+  public isBuilder = false;
+  public customField = new FormControl();
+  public customFieldType = new FormControl('text');
   public form = new FormGroup({
-    name: new FormControl(null, Validators.required),
-    surname: new FormControl(null, Validators.required),
-    phone: new FormControl(null, Validators.required)
+    name: new FormControl(null,[Validators.required, Validators.minLength(2), Validators.maxLength(30), Validators.pattern(/^[a-zA-Z '-]+$/)]),
+    surname: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30), Validators.pattern(/^[a-zA-Z '-]+$/)]),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^(?!\+{2,})(\+[0-9]+)$/)])
   })
 
-  constructor(@Inject(ICONTACTS_SERVICE) public contactsService: ContactsService, private router: Router) { }
+  constructor(@Inject(ICONTACTS_SERVICE) public contactsService: ContactsService, private router: Router) {}
 
-  get name(): FormControl {
-    return (this.form.get('name') as FormControl);
+  getFormControl(controlName: string): FormControl {
+    return (this.form.get(controlName) as FormControl);
   }
 
-  get surname(): FormControl {
-    return (this.form.get('surname') as FormControl);
-  }
-
-  get phone(): FormControl {
-    return (this.form.get('phone') as FormControl);
+  toggleField(): void {
+    this.isBuilder = !this.isBuilder;
+    console.log(this.form)
   }
 
   addField(): void {
-
+    const fieldName = this.customField.value.toLowerCase().replace(' ', '_');
+    this.form.addControl(fieldName, new FormControl());
   }
 
   save(): void {
+    console.log(this.form)
     const contacts = this.contactsService.getContacts();
     const id = this.contactsService.getLastId() + 1;
     const formData = {id, ...this.form.getRawValue()}
